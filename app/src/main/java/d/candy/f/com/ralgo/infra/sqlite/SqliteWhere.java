3206ce416b80ac4099db62ba00a169dea8344e2e
f-candy-d <pagunouetamuramaro@gmail.com>
2017-08-15 14:53:08 +0900

    create expression classes for sql where

--- app/src/main/java/d/candy/f/com/ralgo/infra/sqlite/SqliteWhere.java
+++ app/src/main/java/d/candy/f/com/ralgo/infra/sqlite/SqliteWhere.java
@@ -6,6 +6,7 @@ import android.support.v4.util.Pair;
 import android.text.TextUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by daichi on 8/15/17.
@@ -17,7 +18,36 @@ public class SqliteWhere {
      */
     abstract static class Expr {
         protected static final String SPACE = " ";
+        private boolean mNegation = false;
+        private boolean mIsInBrancket = false;
+
         public Expr() {}
+
+        public boolean isNegation() {
+            return mNegation;
+        }
+
+        public void setNegation(boolean negation) {
+            mNegation = negation;
+        }
+
+        public boolean isInBrancket() {
+            return mIsInBrancket;
+        }
+
+        public void setInBrancket(boolean inBrancket) {
+            mIsInBrancket = inBrancket;
+        }
+
+        protected String toStringConsideringIsInBrancketAndNegation(@NonNull String baseExpression) {
+            if (isNegation()) {
+                baseExpression = "NOT " + baseExpression;
+            }
+
+            return (isInBrancket())
+                    ? "(" + baseExpression + ")"
+                    : baseExpression;
+        }
     }
 
     /**
@@ -31,7 +61,6 @@ public class SqliteWhere {
         }
 
         @NonNull private ArrayList<Pair<Expr, LogicOp>> mOperands;
-        private boolean mIsInBrancket = false;
 
         public LogicExpr(@NonNull Expr mostLeftExpr) {
          mOperands = new ArrayList<>();
@@ -99,14 +128,6 @@ public class SqliteWhere {
             return joinLogicExpr(LogicOp.OR, left, right, LogicOp.OR);
         }
 
-        public boolean isInBrancket() {
-            return mIsInBrancket;
-        }
-
-        public void setInBrancket(boolean inBrancket) {
-            mIsInBrancket = inBrancket;
-        }
-
         public void reset(@NonNull Expr mostLeftExpr) {
             mOperands.clear();
             mOperands.add(new Pair<Expr, LogicOp>(mostLeftExpr, null));
@@ -126,9 +147,7 @@ public class SqliteWhere {
                 expression = expression.concat(SPACE + pair.second.toString() + SPACE + pair.first.toString());
             }
 
-            return (mIsInBrancket)
-                    ? "(" + expression + ")"
-                    : expression;
+            return toStringConsideringIsInBrancketAndNegation(expression);
         }
     }
 
@@ -204,150 +223,16 @@ public class SqliteWhere {
         @Override
         public String toString() {
             if (this.mCondition != null && this.mOperator != null) {
-                return this.mColumn + SPACE + this.mOperator.toString() + SPACE + this.mCondition;
+                String expression = this.mColumn + SPACE + this.mOperator.toString() + SPACE + this.mCondition;
+                return toStringConsideringIsInBrancketAndNegation(expression);
             }
             throw new IllegalStateException("Syntax error");
         }
     }
 
     /**
-     * Other special expression class
+     * BETWEEN expression class
      */
-    public static class SpecExpr extends Expr {
-
-        public enum SpecOp {
-            BETWEEN,
-            NOT_BETWEEN,
-            LIKE,
-            IN,
-            IS_NULL,
-            IS_NOT_NULL;
-
-            public String toString(final ArrayList<String> args) {
-                switch (this) {
-                    case BETWEEN: {
-                        if (2 <= args.size()) {
-                            return "BETWEEN " + args.get(0) + " AND " + args.get(1);
-                        }
-                        throw new IllegalArgumentException("Too few arguments");
-                    }
-
-                    case NOT_BETWEEN: {
-                        if (2 <= args.size()) {
-                            return "NOT BETWEEN " + args.get(0) + " AND " + args.get(1);
-                        }
-                        throw new IllegalArgumentException("Too few arguments");
-                    }
-
-                    case LIKE: {
-                        if (1 <= args.size()) {
-                            return "LIKE " + args.get(0);
-                        }
-                        throw new IllegalArgumentException("Too few arguments");
-                    }
-
-                    case IN: {
-                        if (1 <= args.size()) {
-                            return "IN(" + TextUtils.join(",", args) + ")";
-                        }
-                        throw new IllegalArgumentException("Too few arguments");
-                    }
-
-                    case IS_NULL: {
-                        return "IS NULL";
-                    }
-
-                    case IS_NOT_NULL: {
-                        return "IS NOT NULL";
-                    }
-
-                    default:
-                        throw new IllegalArgumentException();
-                }
-            }
-        }
-
-        @Nullable private SpecOp operator = null;
-        private ArrayList<String> args;
-        public String operand = null;
-
-        public SpecExpr() {
-            // (NOT)BETWEEN requires 2 arguments, LIKE requires 1 argument,
-            // IN requires some arguments more than 0, and IS(NOT)NULL requires no argument, so...
-            this.args = new ArrayList<>(2);
-        }
-
-        public void between(final String operand, final long min, final long max) {
-            this.operator = SpecOp.BETWEEN;
-            this.operand = operand;
-            this.args.clear();
-            this.args.add(String.valueOf(min));
-            this.args.add(String.valueOf(max));
-        }
-
-        public void notBetween(final String operand, final long min, final long max) {
-            this.operator = SpecOp.NOT_BETWEEN;
-            this.operand = operand;
-            this.args.clear();
-            this.args.add(String.valueOf(min));
-            this.args.add(String.valueOf(max));
-        }
-
-        public void like(final String operand, final String regex) {
-            this.operator = SpecOp.LIKE;
-            this.operand = operand;
-            this.args.clear();
-            this.args.add("'" + regex + "'");
-        }
-
-        public void in(final String operand, final String[] vals) {
-            this.operator = SpecOp.IN;
-            this.operand = operand;
-            this.args.clear();
-            for (String val : vals) {
-                this.args.add("'" + val + "'");
-            }
-        }
-
-        public void in(final String operand, final int[] vals) {
-            this.operator = SpecOp.IN;
-            this.operand = operand;
-            this.args.clear();
-            for (int val : vals) {
-                this.args.add(String.valueOf(val));
-            }
-        }
-
-        public void in(final String operand, final long[] vals) {
-            this.operator = SpecOp.IN;
-            this.operand = operand;
-            this.args.clear();
-            for (long val : vals) {
-                this.args.add(String.valueOf(val));
-            }
-        }
-
-        public void isNull(final String operand) {
-            this.operator = SpecOp.IS_NULL;
-            this.operand = operand;
-            this.args.clear();
-        }
-
-        public void isNotNull(final String operand) {
-            this.operator = SpecOp.IS_NOT_NULL;
-            this.operand = operand;
-            this.args.clear();
-        }
-
-        @Override
-        public String toString() {
-            if (this.operand != null && this.operator != null) {
-                return this.operand + SPACE + this.operator.toString(this.args);
-            }
-            return null;
-        }
-    }
-
     public static class BetweenExpr extends Expr {
 
         @NonNull private String mColumn;
@@ -374,13 +259,145 @@ public class SqliteWhere {
         @Override
         public String toString() {
             if (mMin != null && mMax != null) {
-                return mColumn + SPACE + "BETWEEN " + mMin + " AND " + mMax;
+                String expression = mColumn + " BETWEEN " + mMin + " AND " + mMax;
+                return toStringConsideringIsInBrancketAndNegation(expression);
             }
             throw new IllegalStateException("Syntax error");
         }
     }
 
-    public static class LikeExprt extends Expr {
+    /**
+     * LIKE expression class
+     */
+    public static class LikeExpr extends Expr {
 
+        @NonNull private String mColumn;
+        private String mRegex;
+
+        public LikeExpr(@NonNull String column) {
+            this(column, null);
+        }
+
+        public LikeExpr(@NonNull String column, String regex) {
+            mColumn = column;
+            mRegex = regex;
+        }
+
+        @NonNull
+        public String getRegex() {
+            return mRegex;
+        }
+
+        public void setRegex(@NonNull String regex) {
+            mRegex = regex;
+        }
+
+        @NonNull
+        public String getColumn() {
+            return mColumn;
+        }
+
+        public void setColumn(@NonNull String column) {
+            mColumn = column;
+        }
+
+        @Override
+        public String toString() {
+            if (mRegex != null) {
+                String expression = mColumn + " LIKE " + mRegex;
+                return toStringConsideringIsInBrancketAndNegation(expression);
+            }
+            throw new IllegalStateException("Syntax error");
+        }
+    }
+
+    /**
+     * IN expression class
+     */
+    public static class InExpr extends Expr {
+
+        @NonNull private String mColumn;
+        private ArrayList<String> mArgs;
+
+        public InExpr(@NonNull String column) {
+            mColumn = column;
+            mArgs = new ArrayList<>();
+        }
+
+        @SafeVarargs
+        public <T> InExpr(@NonNull String column, @NonNull T... args) {
+            this(column);
+            addArgs(args);
+        }
+
+        public void resetArgs() {
+            mArgs.clear();
+        }
+
+        @SafeVarargs
+        final public <T> void resetArgs(@NonNull T... args) {
+            resetArgs();
+            addArgs(args);
+        }
+
+        @SafeVarargs
+        final public <T> void addArgs(@NonNull T... args) {
+            for (T arg : args) {
+                mArgs.add(arg.toString());
+            }
+        }
+
+        @NonNull
+        public String getColumn() {
+            return mColumn;
+        }
+
+        public void setColumn(@NonNull String column) {
+            mColumn = column;
+        }
+
+        @Override
+        public String toString() {
+            if (1 <= mArgs.size()) {
+                String expression = mColumn + " IN(" + TextUtils.join(",", mArgs) + ")";
+                return toStringConsideringIsInBrancketAndNegation(expression);
+            }
+            throw new IllegalArgumentException("Syntax error");
+        }
+    }
+
+    /**
+     * IS NULL expression class
+     */
+    public static class IsNullExpr extends Expr {
+
+        @NonNull String mColumn;
+
+        public IsNullExpr(@NonNull String column) {
+            mColumn = column;
+        }
+
+        @NonNull
+        public String getColumn() {
+            return mColumn;
+        }
+
+        public void setColumn(@NonNull String column) {
+            mColumn = column;
+        }
+
+        @Override
+        public String toString() {
+            String expression;
+            if (isNegation()) {
+                expression = mColumn + " IS NOT NULL";
+            } else {
+                expression = mColumn + " IS NULL";
+            }
+
+            return (isInBrancket())
+                    ? "(" + expression +")"
+                    : expression;
+        }
     }
 }
